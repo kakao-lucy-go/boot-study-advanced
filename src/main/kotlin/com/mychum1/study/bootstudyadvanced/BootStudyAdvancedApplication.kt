5be7171dfca8bf7b174c75a4f144406f885c8829
mychum1 <want5617@gmail.com>
2021-12-05 14:17:10 +0900

    initialize

---
+++ src/main/kotlin/com/mychum1/study/bootstudyadvanced/BootStudyAdvancedApplication.kt
@@ -0,0 +1,11 @@
+package com.mychum1.study.bootstudyadvanced
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BootStudyAdvancedApplication
+
+fun main(args: Array<String>) {
+	runApplication<BootStudyAdvancedApplication>(*args)
+}
---
+++ src/test/kotlin/com/mychum1/study/bootstudyadvanced/BootStudyAdvancedApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mychum1.study.bootstudyadvanced
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BootStudyAdvancedApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
